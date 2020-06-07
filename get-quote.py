76ef4b5767f854a788b3c2bce0e45a0a0c801f58
shivansh-kaushal <65802533+shivansh-kaushal@users.noreply.github.com>
2020-06-07 15:46:32 +0530

    Update get-quote.py

--- get-quote.py
+++ get-quote.py
@@ -1,4 +1,4 @@
-def main():
+def niam():
   print("keep it logically awesome")
 
   #f = open("quotes.txt")
@@ -7,5 +7,5 @@ def main():
 
   #print(quotes)
 
-if __name__== "__main__":
-  main()
+if __name__== "__miam__":
+  niam()
